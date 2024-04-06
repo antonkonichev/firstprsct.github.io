85ef8f6efe743955c2d6aee54f6ec5b4a787f80a
antonkonichev <118198375+antonkonichev@users.noreply.github.com>
2024-04-06 13:06:34 +0400

    Create style.css

---
+++ style.css
@@ -0,0 +1,347 @@
+header {
+  max-width: 1400px;
+  display: flex;
+  justify-content: space-between;
+  margin: 0 auto;
+  align-items: center;
+}
+.header_nav {
+  display: flex;
+  width: 507px;
+  justify-content: space-between;
+}
+.header_text {
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 20px;
+  color: #18216d;
+  cursor: pointer;
+}
+.header_text:hover {
+  text-decoration: underline;
+}
+.header_login {
+  width: 239px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.header_btn {
+  width: 136px;
+  height: 52px;
+  background: #18216d;
+  border-radius: 6px;
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 17px;
+  line-height: 20px;
+  color: #ffffff;
+  border: none;
+  cursor: pointer;
+}
+.burger {
+  background-image: url(./IMG/burgerIcon.png);
+  width: 28px;
+  height: 12.2px;
+  display: none;
+}
+/** Section */
+.main {
+  max-width: 1188px;
+  height: 519px;
+  margin: 0 auto;
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+  padding: 30px;
+}
+.title {
+  display: block;
+  width: 498px;
+  margin: 0 auto;
+}
+.main_title {
+  width: 498px;
+  height: 120px;
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 48px;
+  line-height: 60px;
+  color: #18216d;
+  margin: 20px auto;
+}
+.main_subtitle {
+  width: 484px;
+  height: 56px;
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 17px;
+  line-height: 28px;
+  color: #18216d;
+  margin: 0 auto;
+}
+.main_btn {
+  width: 180px;
+  height: 66px;
+  background: #18216d;
+  border-radius: 6px;
+  border: none;
+  box-shadow: none;
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 17px;
+  line-height: 21px;
+  color: #ffffff;
+  cursor: pointer;
+  margin: 20px auto;
+}
+.main_btn:hover {
+  background: #00ecd0;
+  color: #18216d;
+  transition: 0.2s;
+}
+.main_img {
+  width: 786px;
+  height: 518.35px;
+  justify-content: right;
+  align-items: center;
+  margin-left: -100px;
+}
+
+/* Секция партнеры и пользователи*/
+.user {
+  width: 100%;
+  max-width: 1140px;
+  height: 234px;
+  margin: 0 auto;
+  padding: 30px;
+}
+.user_top {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 0 auto;
+  padding: 10px;
+}
+.user_text {
+  display: flex;
+  justify-content: left;
+  width: 494px;
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 36px;
+  line-height: 50px;
+  color: #18216d;
+}
+.user_raiting {
+  width: 338px;
+  display: flex;
+  justify-content: space-between;
+}
+.user_partner {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 1110px;
+  margin: 0 auto;
+  padding: 20px;
+}
+.about {
+  width: 100%;
+  max-width: 1120px;
+  height: 459px;
+  margin: 0 auto;
+  padding: 80px;
+}
+.about_text {
+  display: block;
+  align-items: center;
+  justify-content: space-between;
+}
+.about_title {
+  font-family: "Inter";
+  width: 463px;
+  height: 64px;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 48px;
+  line-height: 64px;
+  letter-spacing: -1px;
+  text-align: center;
+  text-transform: capitalize;
+  color: #18216d;
+  margin: 0 auto;
+}
+.about_subtitle {
+  width: 542px;
+  height: 60px;
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 30px;
+  text-align: center;
+  color: #18216d;
+  margin: 0 auto;
+}
+
+/*О нас*/
+.about_more {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  margin-left: 50px;
+  padding: 60px;
+}
+.about_ap {
+  max-width: 320px;
+  height: 267px;
+  margin-right: 20px;
+}
+.about_ap_title {
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 24px;
+  line-height: 29px;
+  color: #18216d;
+}
+
+.about_ap_text {
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 26px;
+  color: #18216d;
+  opacity: 0.6;
+}
+
+.about_ap_lm {
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 16px;
+  color: #18216d;
+  cursor: pointer;
+}
+.about_ap_lm:hover {
+  text-decoration: underline;
+}
+.about_sr {
+  max-width: 320px;
+  height: 275px;
+  margin-right: 20px;
+}
+.about_sr_title {
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 24px;
+  line-height: 29px;
+  color: #18216d;
+}
+
+.about_sr_text {
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 26px;
+  color: #18216d;
+  opacity: 0.6;
+}
+
+.about_sr_lm {
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 16px;
+  color: #18216d;
+}
+.about_sr_lm:hover {
+  text-decoration: underline;
+}
+
+.about_sc {
+  max-width: 320px;
+  height: 275px;
+  margin-right: 20px;
+}
+
+.about_sc_title {
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 24px;
+  line-height: 29px;
+  color: #18216d;
+}
+
+.about_sc_text {
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 26px;
+  color: #18216d;
+  opacity: 0.6;
+}
+
+.about_sc_lm {
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 16px;
+  color: #18216d;
+}
+.about_sc_lm:hover {
+  text-decoration: underline;
+}
+.sli_der {
+  display: block;
+
+  padding: 20px;
+  height: 300px;
+  box-sizing: border-box;
+}
+
+#slider {
+  position: relative;
+  overflow: hidden;
+}
+
+.slide {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.slide img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.slide-content {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: rgba(0, 0, 0, 0.5);
+  color: white;
+  padding: 20px;
+}
